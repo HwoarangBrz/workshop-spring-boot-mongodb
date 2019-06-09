5c007da37475c345f2a618873fc4c3190c43a659
HwoarangBrz <ricardo.s.jorge@hotmail.com>
2019-06-09 17:52:48 -0300

    Search with query methods

--- src/main/java/com/hwoarang/workshopmongo/resources/PostResource.java
+++ src/main/java/com/hwoarang/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.hwoarang.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hwoarang.workshopmongo.domain.Post;
+import com.hwoarang.workshopmongo.resources.util.URL;
 import com.hwoarang.workshopmongo.services.PostService;
 
 @RestController
@@ -19,7 +23,11 @@ public class PostResource {
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
-		Post obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		return ResponseEntity.ok().body(service.findByTitle(URL.decodeParam(text)));
 	}
 }
