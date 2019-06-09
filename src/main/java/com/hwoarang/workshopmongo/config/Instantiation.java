b2b16d10f2683906f9125fa152a0ca371aaf6f0e
HwoarangBrz <ricardo.s.jorge@hotmail.com>
2019-06-09 17:20:36 -0300

    Add comments to posts

--- src/main/java/com/hwoarang/workshopmongo/config/Instantiation.java
+++ src/main/java/com/hwoarang/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.hwoarang.workshopmongo.domain.Post;
 import com.hwoarang.workshopmongo.domain.User;
 import com.hwoarang.workshopmongo.dto.AuthorDTO;
+import com.hwoarang.workshopmongo.dto.CommentDTO;
 import com.hwoarang.workshopmongo.repository.PostRepository;
 import com.hwoarang.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c3));
+		post2.getComments().addAll(Arrays.asList(c2));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
